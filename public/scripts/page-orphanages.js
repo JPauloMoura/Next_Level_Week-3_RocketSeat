643ad9a7d8aa4f6610bef3a0c09460d37b80a519
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-10-15 11:44:30 -0300

    criado: servidor com rotas usando o HBS

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // create icon
 const icon = L.icon({
-    iconUrl:"./public/assets/map-marker.svg",
+    iconUrl:"/assets/map-marker.svg",
     iconSize:[58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2] //popupAnchor
@@ -19,8 +19,8 @@ const popup = L.popup({
     minWidth: 240,
     minHeight: 240
 }).setContent(`Lar das meninas 
-    <a href="orphanage.html?id=1" class="choose-orphanage">
-        <img src="./public/assets/arrow-white.svg">
+    <a href="/orphanage?id=1" class="choose-orphanage">
+        <img src="/assets/arrow-white.svg">
     </a>`
 )
 
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const pages = require('./pages')
+const server = express()
+
+server
+.use(express.static("public"))
+// conf. template
+.set("views", path.join(__dirname, "views"))
+.set("view engine", "hbs")
+// rotas
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+server.listen(3000)
